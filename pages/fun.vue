17bbc12a84ee2ad9b5433d76ecf606fbb81b8975
unknown <albertopopescu@outlook.com>
2021-06-02 21:31:53 +0100

    check if page is rendered server or client side

--- pages/fun.vue
+++ pages/fun.vue
@@ -2,13 +2,17 @@
   <div>
     <span>This is fun</span>
     <v-btn to="/page"> To page page pls</v-btn>
+    <h1>I am rendered on the {{ renderedOn }} side</h1>
   </div>
 </template>
 
 
 <script>
   export default {
-    layout: 'layoutnew'
+    layout: 'layoutnew',
     // page component definitions
+    asyncData() {
+      return { renderedOn: process.client ? 'client' : 'server' }
+    }
   }
 </script>
